8ea0c5a2618e13b4068b31756b2b3ccbf4a55eed
Michael Liao <mliao@systemgroupinc.com>
2016-07-20 13:37:13 -0400

    aa

--- PokemonGo/RocketAPI/Settings.cs
+++ PokemonGo/RocketAPI/Settings.cs
@@ -10,10 +10,9 @@ namespace PokemonGo.RocketAPI
     {
         //Fetch these settings from intercepting the /auth call in headers and body (only needed for google auth)
         public const bool UsePTC = false;
-        public const string PtcUsername = "ASDFGHJKLHA1";
-        public const string PtcPassword = "12LILIHO";
+        public const string PtcUsername = "";
+        public const string PtcPassword = ""
         public const string DeviceId = "cool-device-id";
-        public const string Email = "michael.liao28@gmail.com";
         public const string ClientSig = "fake";
         public const string LongDurationToken = "fakeid";
         public const double DefaultLatitude = 40.764858121285975;
