01cf2d5d3912c8e61bb11d6559da0401b076058a
Michael Liao <mliao@systemgroupinc.com>
2016-07-30 02:16:34 -0400

    changed setting

--- PokemonGo/RocketAPI/Console/Settings.cs
+++ PokemonGo/RocketAPI/Console/Settings.cs
@@ -29,7 +29,7 @@ namespace PokemonGo.RocketAPI.Console
         public static bool DratiniMode = false;
         public static bool UsingIV = false;
         public static bool RenameMode = false;
-        public static bool RecycleMode = true;
+        public static bool RecycleMode = false;
 
         //San Fran 37.80788523279169,-122.41833686828613
         //Sydney -33.86467850677313,151.21024131774902
